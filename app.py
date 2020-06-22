b287cbf68eef58c2f8ccbdb21c9930cc815c4505
59lx <1197574017@qq.com>
2020-06-22 12:09:31 +0800

    upload the code

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def info_handler():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run()
+
+
+
+
